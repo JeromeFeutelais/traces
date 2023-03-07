cf28eb79475b811a61098cfd6cd716947e0d3a02
JeromeFeutelais <jerome@feutelais.com>
2023-03-07 13:19:31 +0100

    User model

---
+++ src/models/user.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema
+
+const User = new schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true,unique: true },
+  password: { type: String, required: true },
+  date: { type: Date, default: Date.now() },
+  validationToken: { type: String, required: true },
+  validated: { type: Boolean, default: false },
+  resetPasswordToken: { type: String, required: false },
+  resetPasswordExpires: { type: Date, required: false },
+});
+
